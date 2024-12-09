ea02f0c746f1f9538ce7a8a220aa9e5a036ad313
danielbeinat <danielbeinat@outlook.es>
2024-12-09 15:11:15 -0300

    add files

--- Backend/index.js
+++ Backend/index.js
@@ -30,5 +30,5 @@ app.use('/api/subscribers', subscriberRoutes);
 // Middleware de errores
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.API_URL || "http://localhost:3000";
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
