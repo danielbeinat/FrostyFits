e8dd2f1104b6e356bb451be1726d4e45636383f8
danielbeinat <danielbeinat@outlook.es>
2024-12-10 15:57:03 -0300

    add files

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,6 @@ import userRoutes from "./routes/userRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import subscriberRoutes from "./routes/subscriberRoutes.js";
 import errorHandler from "./middlewares/errorHandler.js";
-// import helmet from "helmet";
 
 
 const app = express();
@@ -17,17 +16,29 @@ db();
 
 // Middleware
 // app.use(cors());
-app.use(cors({
-    origin: 'https://frostyfits.onrender.com', // Cambia esto por tu dominio de frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Especifica los métodos permitidos
-}));
-// app.use(helmet());
-
-
 // app.use(cors({
-//     origin: process.env.CLIENT_URL || 'http://localhost:3000', // Dominio dinámico según el entorno
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+//     origin: 'https://frostyfits.onrender.com', // Cambia esto por tu dominio de frontend
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Especifica los métodos permitidos
 // }));
+
+
+const allowedOrigins = [
+    'https://frostyfits.onrender.com',
+    'https://frostyfits-admin.onrender.com',
+];
+
+// Configuración de CORS
+app.use(cors({
+    origin: function (origin, callback) {
+        // Permitir solicitudes sin origen (por ejemplo, desde herramientas de desarrollo)
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 app.use(express.json());
 app.use("/images", express.static("upload/images"));
