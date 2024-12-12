93993d8c55f132ffeebdae97cddb185d289ddd01
danielbeinat <danielbeinat@outlook.es>
2024-12-12 16:20:35 -0300

    add files

--- admin/src/config/config.js
+++ admin/src/config/config.js
@@ -1,4 +1,3 @@
-// export const API_URL = "https://frostyfits-backend.onrender.com";
 // export const API_URL = "http://localhost:3000";
 export const API_URL = "https://frosty-fits-backend.vercel.app"
 
