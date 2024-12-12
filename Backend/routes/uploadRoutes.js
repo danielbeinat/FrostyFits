d86471b58b5b9d9133c118053a129bf7b821fb0f
danielbeinat <danielbeinat@outlook.es>
2024-12-12 17:04:03 -0300

    add files

--- Backend/routes/uploadRoutes.js
+++ Backend/routes/uploadRoutes.js
@@ -1,3 +1,39 @@
+// import express from "express";
+// import multer from "multer";
+// import path from "path";
+
+// const router = express.Router();
+
+// // const API_URL = process.env.API_URL || "http://localhost:3000";
+// const API_URL = "https://frostyfi.netlify.app";
+
+
+// // Configuración de almacenamiento para multer
+// const storage = multer.diskStorage({
+//     destination: "./upload/images",
+//     filename: (req, file, cb) => {
+//         cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+//     }
+// });
+
+// const upload = multer({ storage: storage });
+
+// // Ruta para la subida de imágenes
+// router.post("/upload", upload.single("product"), (req, res) => {
+//     if (!req.file) {
+//         return res.status(400).json({ msg: "No file uploaded" });
+//     }
+//     res.json({
+//         success: 1,
+//         // image_url: API_URL + "/images/" + req.file.filename,
+//         image_url: `${API_URL}/images/${req.file.filename}`,
+
+//     });
+// });
+
+// export default router;
+
+
 import express from "express";
 import multer from "multer";
 import path from "path";
@@ -7,10 +43,11 @@ const router = express.Router();
 // const API_URL = process.env.API_URL || "http://localhost:3000";
 const API_URL = "https://frostyfi.netlify.app";
 
-
-// Configuración de almacenamiento para multer
+// Configuración de almacenamiento para multer usando /tmp
 const storage = multer.diskStorage({
-    destination: "./upload/images",
+    destination: (req, file, cb) => {
+        cb(null, '/tmp'); // Cambiado a /tmp para almacenamiento temporal
+    },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
     }
@@ -25,9 +62,8 @@ router.post("/upload", upload.single("product"), (req, res) => {
     }
     res.json({
         success: 1,
-        // image_url: API_URL + "/images/" + req.file.filename,
-        image_url: `${API_URL}/images/${req.file.filename}`,
-
+        // Para obtener la URL, puedes almacenar el archivo en otro lugar o usar un servicio externo
+        image_url: `${API_URL}/images/${req.file.filename}`, // Cambia esto según cómo manejes las imágenes
     });
 });
 
