3cb99991d1e12031319f3eced30a7f9b22db0541
danielbeinat <danielbeinat@outlook.es>
2024-12-13 10:05:50 -0300

    add files

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,61 @@
+// import express from "express";
+// import cors from "cors";
+// import db from "./config/db.js";
+// import dotenv from "dotenv";
+// import authRoutes from "./routes/authRoutes.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import userRoutes from "./routes/userRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+// import subscriberRoutes from "./routes/subscriberRoutes.js";
+// import errorHandler from "./middlewares/errorHandler.js";
+
+
+// const app = express();
+// dotenv.config();
+// db();
+
+// // Middleware
+// // app.use(cors());
+
+
+
+// const allowedOrigins = [
+//     'https://frostyfi.netlify.app',
+//     'https://frostyfits-admin.netlify.app'
+//     // 'https://frostyfits-admin.onrender.com',
+// ];
+
+// // Configuración de CORS
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('No permitido por CORS'));
+//         }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
+
+// app.use(express.json());
+// app.use("/images", express.static("upload/images"));
+
+
+// // Routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api", uploadRoutes);
+// app.use('/api/subscribers', subscriberRoutes);
+
+
+// // Middleware de errores
+// app.use(errorHandler);
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 import express from "express";
 import cors from "cors";
 import db from "./config/db.js";
@@ -9,25 +67,19 @@ import uploadRoutes from "./routes/uploadRoutes.js";
 import subscriberRoutes from "./routes/subscriberRoutes.js";
 import errorHandler from "./middlewares/errorHandler.js";
 
-
 const app = express();
 dotenv.config();
 db();
 
-// Middleware
-// app.use(cors());
-
-
-
+// Middleware de CORS
 const allowedOrigins = [
     'https://frostyfi.netlify.app',
     'https://frostyfits-admin.netlify.app'
-    // 'https://frostyfits-admin.onrender.com',
 ];
 
-// Configuración de CORS
 app.use(cors({
     origin: function (origin, callback) {
+        console.log('Origin:', origin); // Log para ver el origen
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
@@ -40,18 +92,15 @@ app.use(cors({
 app.use(express.json());
 app.use("/images", express.static("upload/images"));
 
-
-// Routes
+// Rutas
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api", uploadRoutes);
 app.use('/api/subscribers', subscriberRoutes);
 
-
 // Middleware de errores
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
