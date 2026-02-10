ff2f9785bb77f30f615ef39a3871a26d8ea60832
danielbeinat <danielbeinat@outlook.es>
2026-02-09 22:08:21 -0300

    update vercel

--- admin/src/config/config.js
+++ admin/src/config/config.js
@@ -1,5 +1,6 @@
 export const API_URL =
     import.meta.env.VITE_API_URL ||
     (import.meta.env.MODE === 'development'
-        ? 'http://localhost:5000'
-        : 'https://frosty-fits-backend.vercel.app');
+        ? 'http://localhost:3000'
+        : 'https://frostyfits.vercel.app'); // Actualizar con tu URL de Vercel después del deploy
+
